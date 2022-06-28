d00ac1b5f8dfa34da0f5bda019f155f96096cf9b
secondEffect <sung2dakk@gmail.com>
2022-06-29 00:10:24 +0900

    .

---
+++ src/network/token.js
@@ -0,0 +1,13 @@
+const TOKEN = 'token';
+
+export default class TokenStorage {
+  getToken() {
+    return localStorage.getItem(TOKEN);
+  }
+  saveToken(token) {
+    localStorage.setItem(TOKEN, token);
+  }
+  clearToken() {
+    localStorage.removeItem(TOKEN);
+  }
+}
